f129abd1641f3b57121aa4e5c99526213c65f1b6
Data <info@realnetworkingsolutions.com>
2023-10-01 12:00:43 -0400

    Commented out lines up code from up to page 56 so I could make sure Rust is working

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,7 @@
 use std::net::TcpListener;
 use actix_web::dev::Server;
 use actix_web::{web, App, HttpResponse, HttpServer};
+// use serde::de::DeserializeOwned;
 
 // We were returning `impl Responder` at the very beginning.
 // We are now spelling out the type explicitly given that we have
@@ -26,25 +27,25 @@ struct FormData {
 //     ) -> Result<Self, Self::Error>;
 // }
 
-impl<T> FormRequest for Form<T>
-where
-    T: DeserializeOwned + 'static,
-{
-    type Error = actix_web::Error;
-
-    async fn from_request(
-        req: &HttpRequest,
-        payload: &mut Payload
-) -> Result<Self, Self::Error> {
-    // Omitted stuff around extractor configuration (e.g. payload size limits)
-    match UrlEncoded::new(req, payload).await {
-        Ok(item) -> Ok(Form(item)),
-        // The error handler can be customised
-        // The default one will return a 400, which is what we want.
-        Err(e) => Err(error_handler(e))
-        }
-    }
-}
+// impl<T> FormRequest for Form<T>
+// where
+//     T: DeserializeOwned + 'static,
+// {
+//     type Error = actix_web::Error;
+//
+//     async fn from_request(
+//         req: &HttpRequest,
+//         payload: &mut Payload
+// ) -> Result<Self, Self::Error> {
+//     // Omitted stuff around extractor configuration (e.g. payload size limits)
+//     match UrlEncoded::new(req, payload).await {
+//         Ok(item) => Ok(Form(item)),
+//         // The error handler can be customised
+//         // The default one will return a 400, which is what we want.
+//         Err(e) => Err(error_handler(e))
+//         }
+//     }
+// }
 
 // Let's start simple: we always return a 200 OK
 async fn subscribe(_form: web::Form<FormData>) -> HttpResponse {
