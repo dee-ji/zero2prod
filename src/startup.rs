a34fd66b5b4689895a53da7c7bae38f05d043846
Data <info@realnetworkingsolutions.com>
2023-11-23 18:56:47 -0500

    Finished today on page 236. All tests work and project compiles. Working on Error status codes next.

--- src/startup.rs
+++ src/startup.rs
@@ -1,5 +1,6 @@
 //! src/routes/startup.rs
 use crate::routes::{health_check, subscribe};
+use crate::email_client::EmailClient;
 use actix_web::dev::Server;
 use actix_web::web::Data;
 use actix_web::{web, App, HttpServer};
@@ -8,10 +9,13 @@ use std::net::TcpListener;
 use tracing_actix_web::TracingLogger;
 
 pub fn run(
-    listener: TcpListener, db_pool: PgPool
+    listener: TcpListener,
+    db_pool: PgPool,
+    email_client: EmailClient,
 ) -> Result<Server, std::io::Error> {
     // Wrap the pool using web::Data, which boils down to an Arc smart pointer
     let db_pool = Data::new(db_pool);
+    let email_client = Data::new(email_client);
     // Capture `connection` from the surrounding environment
     let server = HttpServer::new(move || {
         App::new()
@@ -21,6 +25,7 @@ pub fn run(
             // A new entry in our routing table for POST /subscriptions requests
             .route("/subscriptions", web::post().to(subscribe))
             .app_data(db_pool.clone())
+            .app_data(email_client.clone())
     })
     .listen(listener)?
     .run();
